d53fb5f0da4388403d98d6df8c716ec57f69a75c
Mark Henderson <mark@mrh.io>
2021-08-03 15:55:02 -0400

    feat: add examples/app.js

--- example/app.js
+++ example/app.js
@@ -1,6 +1,29 @@
-function postMessageToIFrame (e) {
-  const frame = document.getElementById('fcl-adapter-frame')
-  frame.contentWindow.postMessage({ message: 'hi' }, '*', [])
-}
+const frame = document.getElementById('fcl-adapter-frame')
+console.log(frame)
 
-document.getElementById('postMessageButton').addEventListener('click', postMessageToIFrame)
+document.getElementById('authn-button').addEventListener('click', () => {
+  frame.contentWindow.postMessage({
+    type: 'FCL:ADAPTER:AUTHN',
+    authToken: 'dummy-token'
+  }, 'http://localhost:4000')
+})
+
+document.getElementById('authz-button').addEventListener('click', () => {
+  frame.contentWindow.postMessage({ type: 'FCL:ADAPTER:AUTHZ' }, 'http://localhost:4000')
+})
+
+window.addEventListener('message', (message) => {
+  switch (message.data?.type) {
+    case 'FCL:FRAME:READY':
+      console.log('ready!')
+      break
+    case 'FCL:ADAPTER:AUTHN:RESPONSE':
+      console.log(message.data)
+      break
+    case 'FCL:ADAPTER:AUTHZ:RESPONSE':
+      console.log(message.data)
+      break
+    default:
+      break
+  }
+}, false)
