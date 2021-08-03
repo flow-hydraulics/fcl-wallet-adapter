7230565143693c95f81abab66f397f8d0b3b0bc5
Mark Henderson <mark@mrh.io>
2021-08-03 15:55:38 -0400

    feat(adapter): scaffold out events and iframe src handling

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,40 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
+
 function HomePage() {
+  useEffect(() => {
+    const target = 'http://' + window.location.search.substr(1)
+
+    async function handleMessage (msg) {
+      switch (msg.data.type) {
+        case 'FCL:ADAPTER:AUTHN':
+          const resAuthn = await fetch('/api/authn', { method: 'POST' })
+          window.parent.postMessage({
+            type: 'FCL:ADAPTER:AUTHN:RESPONSE',
+            res: (await resAuthn.json())
+          }, target)
+          break
+        case 'FCL:ADAPTER:AUTHZ':
+          const resAuthz = await fetch('/api/authz', { method: 'POST' })
+          window.parent.postMessage({
+            type: 'FCL:ADAPTER:AUTHZ:RESPONSE',
+            res: (await resAuthz.json())
+          }, target)
+          break
+        default:
+          break
+      }
+    }
+
+    window.addEventListener('message', handleMessage)
+    window.parent.postMessage({ type: 'FCL:FRAME:READY' }, target)
+
+    return () => {
+      window.removeEventListener('message', handleMessage)
+    }
+  })
+
   return <div>Welcome to Next.js!</div>
 }
 
